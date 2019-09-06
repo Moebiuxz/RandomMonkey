693c58e183a7b4e258a54352971633f6796ec4f3
Diego Lucero Verdugo <lucero.inginfo@gmail.com>
2019-09-05 23:12:43 -0400

    Se diseña servicio [MonkeyService] y sus funciones

--- src/app/providers/monkey.service.ts
+++ src/app/providers/monkey.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {Monkey} from '../model/monkey';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonkeyService {
+  private readonly resultset: AngularFireList<Monkey>;
+  public selectedMonkey: Monkey = new Monkey();
 
-  constructor() { }
+  constructor(database: AngularFireDatabase) {
+    this.resultset = database.list('monkey');
+  }
+
+  public getMonkeys(): AngularFireList<Monkey> {
+    return this.resultset;
+  }
+
+  public addMonkey(monkey: Monkey) {
+    this.resultset.push(monkey).then(t => {
+      monkey.key = t.key;
+      this.resultset.update(t.key, monkey);
+    });
+  }
+
+  public updateMonkey(key: string, monkey: Monkey) {
+    this.resultset.update(key, monkey);
+  }
+
+  public deleteMonkey(key: string) {
+    this.resultset.remove(key);
+  }
+
+  public deleteEverything() {
+    this.resultset.remove();
+  }
 }
